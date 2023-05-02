78da64c9993a3596528c6d6629ecf615ce37bbb8
Pratik Jangam <pratikjangam57@gmail.com>
2023-05-02 23:41:17 +0530

    Add files via upload

---
+++ Maven/productweb/src/main/java/com/pratik/product/servlets/DisplayProductDetailsServlet.java
@@ -0,0 +1,37 @@
+package com.pratik.product.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pratik.product.BO.ProductBOImpl;
+import com.pratik.product.dto.Product;
+
+/**
+ * Servlet implementation class DisplayProductDetailsServlet
+ */
+public class DisplayProductDetailsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		ProductBOImpl bo = new ProductBOImpl();
+		Product product = bo.findProduct(Integer.parseInt(request.getParameter("id")));
+		PrintWriter out = response.getWriter();
+
+		out.print("Product details");
+		out.print("Product Id: " + product.getId());
+		out.print("Product Name: " + product.getName());
+		out.print("Product Description: " + product.getDescription());
+		out.print("Product Price: " + product.getPrice());
+
+	}
+
+}
